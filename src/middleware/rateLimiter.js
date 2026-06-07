7b071caa98da5b5bbface9cd83eb44c07ab3071a
Kartik-Holkar <290070071+Kartik-Holkar@users.noreply.github.com>
2026-06-06 23:04:09 -0300

    feat: add rate limiter middleware with algorithm selection

---
+++ src/middleware/rateLimiter.js
@@ -0,0 +1,5 @@
+const fixedWindow = require('../algorithms/fixedWindow')
+const slidingWindow = require('../algorithms/slidingWindow')
+const tokenBucket = require('../algorithms/tokenBucket')
+const db = require('../config/db')
+
